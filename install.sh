67d52275878be75e210cacc834625961a2925cde
Apurba Kumar Ganguly <sahebganguly1234@gmail.com>
2021-02-13 11:45:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl status mongod
-sudo systemctl enable mongod
+
 
 sudo apt install -y nodejs
 sudo apt install -y npm
